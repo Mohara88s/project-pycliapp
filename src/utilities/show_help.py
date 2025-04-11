f070e8d2d843ec5a5e003155e32fb51a1e1433e8
Mohara88s <mohara88s@gmail.com>
2025-04-11 20:01:33 +0300

    colorize help

--- src/utilities/show_help.py
+++ src/utilities/show_help.py
@@ -1,9 +1,11 @@
 from utilities.colorize import colorize_message
 
 def show_help(commands):
+        i=0
         print("This is available commands:")
         for name, info in commands.items():
-            print(f"  {name:<15} - {info['description']}")
-    
+            print(colorize_message(f"  {name:<15} - {info['description']}", f"{"CYAN" if i%2==0 else "BLUE"}"))
+            i+=1
+
 if __name__ == "__main__":
     pass
