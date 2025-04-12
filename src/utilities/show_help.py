71697cdcab7a12104f014bf8b2ac30f3a2ed2cbe
Mohara88s <mohara88s@gmail.com>
2025-04-12 11:01:52 +0300

    little fix help color

--- src/utilities/show_help.py
+++ src/utilities/show_help.py
@@ -2,7 +2,7 @@ from utilities.colorize import colorize_message
 
 def show_help(commands):
         i=0
-        print("This is available commands:")
+        print(colorize_message("This is available commands:", "GREEN"))
         for name, info in commands.items():
             print(colorize_message(f"  {name:<15} - {info['description']}", f"{"CYAN" if i%2==0 else "BLUE"}"))
             i+=1
